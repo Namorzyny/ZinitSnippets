cc6b31f23c456396e62298e1825d9f103028d7b8
Namorzyny <namorzyny@gmail.com>
2023-04-30 15:31:31 +0800

    Update omz.zsh

--- snippets/omz.zsh
+++ snippets/omz.zsh
@@ -1,10 +1,9 @@
 zinit snippet OMZL::completion.zsh
 zinit snippet OMZL::directories.zsh
-
-zstyle ':completion:*' list-colors "$LS_COLORS"
-
 zinit snippet OMZL::history.zsh
 zinit snippet OMZL::key-bindings.zsh
 zinit snippet OMZP::colored-man-pages
 zinit snippet OMZP::common-aliases
 zinit snippet OMZP::sudo
+zstyle ':completion:*' list-colors "$LS_COLORS"
+alias ls='ls --color -F'
